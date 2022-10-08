7c93979986e240167a9a65b5718719be8cd036fa
ssteggell <spencer.steggell@gmail.com>
2022-10-08 14:54:52 -0700

    Pushing travis file

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-
+console.log(process.env)
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
